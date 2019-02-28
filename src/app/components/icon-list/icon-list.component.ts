450e43e7f9c13a34e58125666f49bb51b680baa9
klkavyashree <kavya.kavyashree97@gmail.com>
2019-02-28 19:54:24 +0530

    fundoo

--- src/app/components/icon-list/icon-list.component.ts
+++ src/app/components/icon-list/icon-list.component.ts
@@ -7,7 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IconListComponent implements OnInit {
 flag=false;
+colorarray=[[{ 'color': '#FFFFFF', 'name': 'White' },
+{ 'color': '#E53935', 'name': 'Red' },
+{ 'color': '#EF6C00', 'name': 'Orange' },
+{ 'color': '#FFEB3B', 'name': 'Yellow' }],
 
+[{ 'color': '#B2FF59', 'name': 'green' },
+{ 'color': '#69F0AE', 'name': 'teal' },
+{ 'color': '#81D4FA', 'name': 'blue' },
+{ 'color': '#0288D1', 'name': 'darkblue' }],
+
+[{ 'color': '#B39DDB', 'name': 'purple' },
+{ 'color': '#F48FB1', 'name': 'pink' },
+{ 'color': '#FFAB40', 'name': 'brown' },
+{ 'color': '#E0E0E0', 'name': 'gray' }
+
+]]
   constructor() { }
 
   ngOnInit() {
@@ -15,5 +30,5 @@ flag=false;
   remind($event:any){
     this.flag=true;
   }
-  
+ 
 }
---
+++ src/app/service/dataservice/data.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private messageSource = new BehaviorSubject('');//it will always return the current value on subscription no need of callnext 
+  //and ensure that it will always recieve recent data
+  currentmessage=this.messageSource.asObservable();//variable handles the data stream as an observable
+
+  constructor() { }
+  changemessage(message:string){
+    this.messageSource.next(message)
+  }
+}
